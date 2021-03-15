bf02f230cc226452b818741053916787d9f4feaf
Savio Fonseca <savifon1@gmail.com>
2021-03-15 14:04:41 +0000

    iniciando leitura de arquivo TXT

--- Main.java
+++ Main.java
@@ -5,10 +5,18 @@ Ex.: 1,189.90
 4,55.00
 No exemplo acima, a filial 1 fez duas vendas, a primeira totalizando R$ 189,90 e a segunda R$ 45,70. A filial 3 fez uma venda de R$ 29,90 e a 4 também uma de R$ 55,00. Faça um programa que leia este arquivo e informe, ao final, o total e o valor médio das vendas de cada filial. */
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     
-		
+		Scanner scanner = new Scanner(new FileReader("vendas.txt")).useDelimiter("\n");
+
+		while (scanner.hasNext()) {
+			System.out.println( scanner.next() );
+		}
 
   }
 }
