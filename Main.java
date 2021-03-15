72ba33acb0fc06168b00b96f443a5bdce43ee488
Savio Fonseca <savifon1@gmail.com>
2021-03-15 13:42:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+/* Uma loja possui 4 filiais, cada uma com um código de 1 a 4. Um arquivo contendo todas as vendas feitas durante o dia nas quatro filiais é gerado a partir de uma planilha, sendo que cada linha do arquivo contém o número da filial e o valor de uma venda efetuada, separados por vírgula.
+Ex.: 1,189.90
+1,45.70
+3,29.90
+4,55.00
+No exemplo acima, a filial 1 fez duas vendas, a primeira totalizando R$ 189,90 e a segunda R$ 45,70. A filial 3 fez uma venda de R$ 29,90 e a 4 também uma de R$ 55,00. Faça um programa que leia este arquivo e informe, ao final, o total e o valor médio das vendas de cada filial. */
+
+class Main {
+  public static void main(String[] args) {
+    
+		
+
+  }
+}
